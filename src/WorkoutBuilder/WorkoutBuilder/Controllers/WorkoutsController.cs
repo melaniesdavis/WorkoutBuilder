6ac4cb5ef889182fe27aac720ea04023755f407b
melaniesdavis <melaniesdavis@hotmail.com>
2017-02-24 11:42:30 -0500

    Added Workout view, controller.

---
+++ src/WorkoutBuilder/WorkoutBuilder/Controllers/WorkoutsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkoutBuilder.Data;
+
+namespace WorkoutBuilder.Views.Workouts
+{
+    public class WorkoutsController : Controller
+    {
+        // GET: Workouts
+        public ActionResult Index()
+        {
+            var workouts = Repository.GetWorkouts();
+
+            return View(workouts);
+        }
+    }
+}
