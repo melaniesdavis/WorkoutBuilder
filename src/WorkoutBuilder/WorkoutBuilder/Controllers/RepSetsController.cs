c1687ab34e49834cfd54ca99d2dcbbc5c4ce2667
melaniesdavis <melaniesdavis@hotmail.com>
2017-03-01 16:52:36 -0500

    Modifying the look of Exercises Index view.

---
+++ src/WorkoutBuilder/WorkoutBuilder/Controllers/RepSetsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkoutBuilder.Data;
+
+namespace WorkoutBuilder.Controllers
+{
+    public class RepSetsController : Controller
+    {
+        // GET: RepSets
+        public ActionResult Index()
+        {
+            var repset = Repository.GetRepSet();
+
+            return View(repset);
+        }
+    }
+}
