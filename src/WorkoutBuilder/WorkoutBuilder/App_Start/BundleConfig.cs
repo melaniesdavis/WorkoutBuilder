6799d176a0c2a25e255294d0762c85afd8da6d02
melaniesdavis <melaniesdavis@hotmail.com>
2017-03-04 16:23:14 -0500

    Playing with layout of Exercises Index

--- src/WorkoutBuilder/WorkoutBuilder/App_Start/BundleConfig.cs
+++ src/WorkoutBuilder/WorkoutBuilder/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace WorkoutBuilder
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cyborg.css",
                       "~/Content/site.css"));
         }
     }
